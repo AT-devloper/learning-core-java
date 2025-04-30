20b30a2ce38e9f34f2408edd3953af85d387afd5
AT-devloper <ayushap.poddar@gmail.com>
2025-04-30 16:28:46 +0530

    30apl

---
+++ Two.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+
+public class Two
+{
+    public static void main(String ar[])
+    {
+        // int a ='A';
+        // char c='c';
+        // float f =1.22222f;
+        // double d=1.2222222222;
+        // boolean b=true;
+        // short s=16;
+        // long l = 2222;
+
+        // System.out.println("int:- " +a);
+        // System.out.println("char:- " +c);
+        // System.out.println("float:- " +f);
+        // System.out.println("Double:- " +d);
+        // System.out.println("boolean:- " +b);
+        // System.out.println("Short:- " +s);
+        // System.out.println("long:- " +l);
+
+
+
+    Scanner obj=new Scanner(System.in);
+
+    System.out.print("enter value :- ");
+    int a=obj.nextInt();
+    
+    System.out.print("enter value :- ");
+    int b=obj.nextInt();
+
+    int add = a+b;
+    int subs =b-a;
+    int multi =a*b;
+    int divid=a/b;
+    int rem=a%b;
+
+
+    System.out.println("Addition of this values is "+add);
+    System.out.println("Substraction of this values is "+subs);
+    System.out.println("Multiply of this values is "+multi);
+    System.out.println("Division of this values is "+divid);
+    System.out.println("Moduler of this values is "+rem);
+
+
+    }
+      
+}
