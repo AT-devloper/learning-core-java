d35d24bae827eaa4691de198ebd03769f364bd49
AT-devloper <ayushap.poddar@gmail.com>
2025-04-30 15:54:25 +0530

    30apl

--- One.java
+++ One.java
@@ -1,7 +1,57 @@
+import java.util.Scanner;
+
 public class One
 {
     public static void main(String ar[])
     {
-        System.out.print("hello");
+    // System.out.println("hello budy");
+    // System.out.println("hii dude");
+    // System.out.println("Welcome's you");
+
+    // Scanner obj=new Scanner(System.in);
+    // System.out.println("enter value");
+    // int a;
+    // a=obj.nextInt();
+    // System.out.println(a);
+
+    // Scanner obj=new Scanner(System.in);
+    // System.out.println("enter value");
+    // int a=obj.nextInt();
+    // System.out.println(a);
+
+    // System.out.println("enter value");
+    // int b=obj.nextInt();
+    // System.out.println(b);
+
+ Scanner obj=new Scanner(System.in);
+
+    System.out.print("enter value :- ");
+    int a=obj.nextInt();
+    
+    System.out.print("enter value :- ");
+    int b=obj.nextInt();
+
+    int c = a+b;
+
+    System.out.println("Addition of this values is "+c);
+   
+
     }
 }
+
+
+// class PrintStream
+// {
+//     void print(String a)
+//     {
+
+//     }
+// }
+// class System{
+//     static PrintStream out;
+// }
+
+// System.out.print("hello")
+
+
+
