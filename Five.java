441594a40819b4b19c2ae9661033b2d0ee95b582
AT-devloper <ayushap.poddar@gmail.com>
2025-05-04 22:30:15 +0530

    3may

--- Five.java
+++ Five.java
@@ -1,18 +1,42 @@
-//3 may 2025
-//
+// 3 may 2025
+// taking input
 
-
+import java.util.Scanner;
 class Five
+
 {
     public static void main(String ar[])
     {
-        
+        Scanner obj=new Scanner(System.in);
 
+        // 5 WAYS TO GET INPUT FROM USER
 
+            System.out.print("enter int value :- ");
+            int a=obj.nextInt();
+            System.out.println(a);
 
+            System.out.print("enter float value :- ");
+            float f =obj.nextFloat();
+            System.out.println(f);
+
+            System.out.print("enter double value :- ");
+            double d =obj.nextDouble();
+            System.out.println(d);
+
+            System.out.print("enter string value :- ");
+            String s=obj.next();
+            System.out.println(s);
+
+            System.out.print("enter char value :- ");
+            char c =obj.next().charAt(0);
+            System.out.println(c);
 
     }
 }
+
+//  Hw
+//  about jdk
+//  5 ways to get input 
 
 
 
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,6 +1,3 @@
-int ans =(num>12 && num<20)?num:0;
-    System.out.println(ans);
-    System.out.println((num>9 && num<20)?num:"!!");
-    //direct print string  ans integer
-
-    System.out.println(((num>10 && num<20) && (num%2==0 && num%3==0)) ? num :"!!" );
+System.out.print("enter value :- ");
+            float f =Scanner.nextfloat();
+            System.out.print(f);
