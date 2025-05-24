70532986f4c99f3baf7043bd3b20ce81d7719f9e
AT-devloper <ayushap.poddar@gmail.com>
2025-05-24 08:47:43 +0530

    24may

--- classes/Sixteen.java
+++ classes/Sixteen.java
@@ -1,5 +1,5 @@
-//    may 2025
-//
+//   23 may 2025
+//   pattens
 
 
 
@@ -7,6 +7,183 @@ class Sixteen
 {
     public static void main(String ar[])
     {
+        // int a=0;
+        // for(int r=1;r<=9;++r){
+        //     a=(r<=5)?++a:--a;                //diamond
+        //     for(int c=1;c<=9;++c)
+        //     {
+        //         if(c>=6-a && c<=4+a)
+        //         {
+        //             System.out.print("^");
+        //         }
+        //         else{System.out.print(" ");}
+        //     }
+        //     System.out.println();
+        // }
+        
+        // int a=0;
+        // for(int r=1;r<=9;++r)           //diamond
+        // {
+        //     a=(r<=5)?++a:--a;
+
+        //     for(int c=1;c<=9;++c){
+
+        //     if(c>=6-a && c<=4+a)
+        //     {
+        //         System.out.print("$");
+        //     }
+        //     else {System.out.print(" ");}
+        //     }
+        //     System.out.println();
+        // }
+
+//         for(int r = 1 ; r<=5 ; ++r)         //Right angle
+//         {
+//             for(int c =1 ; c<=5 ; ++c)
+//             {
+//                 if(c<=r)                //row
+//                 {
+//                     System.out.print(r);
+//                 }
+//                 else{System.out.print(" ");}
+
+//             }
+//             System.out.println();
+//         }
+
+        
+//  System.out.println();                     
+//         int n=0;
+//         for(int r = 1 ; r<=5 ; ++r)              // left angle
+//         {
+//             for(int c =1 ; c<=5 ; ++c)
+//             {
+//                 if(c>=6-r)
+//                 {
+//                     System.out.print(++n);
+//                 }
+//                 else{System.out.print(" ");}
+
+//             }
+//             System.out.println();
+//         }
+
+// System.out.println();  
+//  for(int r = 1 ; r<=5 ; ++r)         //Right angle
+//         {
+//             for(int c =1 ; c<=5 ; ++c)
+//             {
+//                 if(c<=r)              // column
+//                 {
+//                     System.out.print(c);
+//                 }
+//                 else{System.out.print(" ");}
+
+//             }
+//             System.out.println();
+//         }
+
+
+//         System.out.println();                     
+//         char ab='a';
+//         for(int r = 1 ; r<=5 ; ++r)              // left angle
+//         {
+//             for(int c =1 ; c<=5 ; ++c)
+//             {
+//                 if(c>=6-r)
+//                 {
+//                     System.out.print(ab++);
+//                 }
+//                 else{System.out.print(" ");}
+
+//             }
+//             System.out.println();
+//         }
+
+
+//         System.out.println();                     
+//         char AB='A';
+//         for(int r = 1 ; r<=5 ; ++r)              // right angle
+//         {
+//             for(int c =1 ; c<=5 ; ++c)
+//             {
+//                 if(c<=r)
+//                 {
+//                     System.out.print(AB++);
+//                 }
+//                 else{System.out.print(" ");}
+
+//             }
+//             System.out.println();
+//         }
+
+ System.out.println();                     
+     
+        for(int r = 1 ; r<=5 ; ++r)              // right angle
+        {
+            for(int c =1 ; c<=5 ; ++c)
+            {
+                if(c<=r)
+                {
+                    System.out.print((char)(r+64));
+                }
+                else{System.out.print(" ");}
+
+            }
+            System.out.println();
+        }
+
+
+         System.out.println();                     
+     
+        for(int r = 1 ; r<=5 ; ++r)              // right angle
+        {
+            for(int c =1 ; c<=5 ; ++c)
+            {
+                if(c<=r)
+                {
+                    System.out.print((char)(r+96));
+                }
+                else{System.out.print(" ");}
+
+            }
+            System.out.println();
+        }
+
+         System.out.println();                     
+     
+        for(int r = 1 ; r<=5 ; ++r)              // right angle
+        {
+            for(int c =1 ; c<=5 ; ++c)
+            {
+                if(c<=r)
+                {
+                    System.out.print((char)(c+64));
+                }
+                else{System.out.print(" ");}
+
+            }
+            System.out.println();
+        }
+
+ System.out.println();                     
+     
+        for(int r = 1 ; r<=5 ; ++r)              // right angle
+        {
+            for(int c =1 ; c<=5 ; ++c)
+            {
+                if(c<=r)
+                {
+                    System.out.print((char)(c+96));
+                }
+                else{System.out.print(" ");}
+
+            }
+            System.out.println();
+        }
+
+        
+
 
 
 
