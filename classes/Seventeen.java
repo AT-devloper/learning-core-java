8e178d2d2427ba198de17ff89107b2ac60069b1e
AT-devloper <ayushap.poddar@gmail.com>
2025-05-27 17:54:32 +0530

    27may

--- classes/Seventeen.java
+++ classes/Seventeen.java
@@ -2,22 +2,23 @@
 //  
 
 
-class Seventeen
+public class Seventeen
 {
     public static void main(String ar[])
     { 
-
-
-        
-
-
-
-
-
-
-
+        int a=10,b=20;
+        System.out.print(a+b);
     }
+
+    public static void multi(String ar[])
+    {
+        int c=10,d=20;
+        System.out.print(c*d);
+    }
+
+
 }
+
 
 
 // what is java c 
--- classes/tempCodeRunnerFile.java
+++ classes/tempCodeRunnerFile.java
@@ -1,10 +1,5 @@
-
-        int  num=6;                     
-        int sum =0;                             // Perfect number
-        for (int i=1;i<num;++i)
-        {
-            if(num%i==0) sum=sum+i;
-        }
-        if(sum==num)  System.out.println("Perfect number");
-        
-        else{ System.out.println("Not perfect number");}
+public static void main(String ar[])
+    // { 
+    //     int a=10,b=20;
+    //     System.out.print(a+b);
+    // }
