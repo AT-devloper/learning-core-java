4cbd9f2807b25beaab16ebc1092053565f58d659
AT-devloper <ayushap.poddar@gmail.com>
2025-05-28 17:27:49 +0530

    28may

--- classes/Seventeen.java
+++ classes/Seventeen.java
@@ -1,20 +1,24 @@
 //  27 may 2025
-//  
+//  know about javac and java
 
 
 public class Seventeen
 {
-    public static void main(String ar[])
-    { 
-        int a=10,b=20;
-        System.out.print(a+b);
-    }
+   
+}
 
-    public static void multi(String ar[])
+public class Multi
+{
+    public static void multi()
     {
         int c=10,d=20;
         System.out.print(c*d);
     }
+
+ public static void main(String ar[])
+    { 
+        Multi.multi();
+    } 
 
 
 }
