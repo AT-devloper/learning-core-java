436efff656cba3358488ce03a75eda932e1da128
AT-devloper <ayushap.poddar@gmail.com>
2025-05-29 17:48:14 +0530

    29may

--- Practice/Prc_5.java
+++ Practice/Prc_5.java
@@ -17,58 +17,58 @@ class Prc_5
         //     }
         //     System.out.println("Prime");
 
-    // int a=1,b=2;
-    // a=a+b; //3
-    // b=a-b; //2     // swap value
-    // a=a-b; // 1
-    // System.out.println(a);
-    // System.out.println(b);
+        // int a=1,b=2;
+        // a=a+b; //3
+        // b=a-b; //2     // swap value
+        // a=a-b; // 1
+        // System.out.println(a);
+        // System.out.println(b);
 
-    // String number="123456";
-    // int num=Integer.parseInt(number);
-    // int sum=0;
-    //                             // String to Integer & fing sum of the integer?
-    // while(num>0)
-    // {
-    //     int digit=num%10;
-    //     sum=sum+digit;
-    //     num=num/10;
+        // String number="123456";
+        // int num=Integer.parseInt(number);
+        // int sum=0;
+        //                             // String to Integer & fing sum of the integer?
+        // while(num>0)
+        // {
+        //     int digit=num%10;
+        //     sum=sum+digit;
+        //     num=num/10;
 
-    // }
-    // System.out.print(sum);
-
-
-    // int nu=3;                                        // bit value of 3 = (0011) 
-    // if((nu & 1)==0) System.out.println("EVEN");      // ((3 & 1)==0 ) = ((0011 & 0001)==0 ) false;
-    // else System.out.println("ODD");
-
-    //  Scanner sc=new Scanner(System.in);
-    //  System.out.print("Enter Day :- ");
-    //  int day=sc.nextInt();
-    //  switch(day)
-    //  {
-    //     case 1 -> System.out.print("Monday");
-    //     case 2 -> System.out.print("Tuesday");
-    //     case 3 -> System.out.print("Wednesday");
-    //     case 4 -> System.out.print("Thrusday");
-    //     case 5 -> System.out.print("Friday");
-    //     case 6 -> System.out.print("Suterday");
-    //     case 7 -> System.out.print("Sunday");
-
-    //     default -> System.out.println(" !! ");
-
-    // }
-
-// int a=10 ,b=4 ,c=8;
-// int lg=(a > b) ? ( a > c ? a : c ) : ( b > c ? b : c );
-// System.out.println(lg);
+        // }
+        // System.out.print(sum);
 
 
-int per=20;
-if(per>=90) System.out.println("A");
-else if (per>=75) System.out.println("B");
-else if (per>=50) System.out.println("C");
-else System.out.println("F");
+        // int nu=3;                                        // bit value of 3 = (0011) 
+        // if((nu & 1)==0) System.out.println("EVEN");      // ((3 & 1)==0 ) = ((0011 & 0001)==0 ) false;
+        // else System.out.println("ODD");
+
+        //  Scanner sc=new Scanner(System.in);
+        //  System.out.print("Enter Day :- ");
+        //  int day=sc.nextInt();
+        //  switch(day)
+        //  {
+        //     case 1 -> System.out.print("Monday");
+        //     case 2 -> System.out.print("Tuesday");
+        //     case 3 -> System.out.print("Wednesday");
+        //     case 4 -> System.out.print("Thrusday");
+        //     case 5 -> System.out.print("Friday");
+        //     case 6 -> System.out.print("Suterday");
+        //     case 7 -> System.out.print("Sunday");
+
+        //     default -> System.out.println(" !! ");
+
+        // }
+
+        // int a=10 ,b=4 ,c=8;
+        // int lg=(a > b) ? ( a > c ? a : c ) : ( b > c ? b : c );
+        // System.out.println(lg);
+
+
+        int per=20;
+        if(per>=90) System.out.println("A");
+        else if (per>=75) System.out.println("B");
+        else if (per>=50) System.out.println("C");
+        else System.out.println("F");
 
 
 
