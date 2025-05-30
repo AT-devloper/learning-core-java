5674011aad4104a6f9d3d2fb8b639bb2fe462aa3
AT-devloper <ayushap.poddar@gmail.com>
2025-05-30 18:13:26 +0530

    30may

---
+++ classes/Eighteen.java
@@ -0,0 +1,42 @@
+// 30 May 2025
+// Array
+
+
+class Eighteen
+{
+    public static void main(String ar[])
+    {
+
+            // Declaration:- 
+            int a;
+                int arr1[];
+                    int[] arr2;
+                        int []arr3;  
+
+            // Invalid:- 
+                // int arr[5];
+
+
+            // Instance :-
+                int[] arr4 = new int[5];
+
+            // Invalid:- 
+                // int[] arr = new int[]; 
+            
+
+            // initialization :-
+                int arr5[]={1,2,3,4,5};
+                    int arr6[]=new int[]{1,2,3,4,5,6};
+
+            // Invalid:- 
+                // int arr[5]={1,2,3,4,5}
+                    // int arr[]=new int[5]{1,2,3,4,5,6}
+    }
+}
+
+// Array is a object.
+// To stpre Multiple element (1,2,3,4,5) of same Datatypes;
+// Array stored where in Stack/in Heap ?  - Heap.
+// Array - Declaration,intanstistion,initialozation,destruction. 
+
+// JAVA COMPLETE's LACK's OF ALL LAGUAGES.
