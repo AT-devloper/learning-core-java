c71441bc185bcf7b5a7a71d348b52aa15c448345
AT-devloper <ayushap.poddar@gmail.com>
2025-06-06 09:07:23 +0530

    6may

--- classes/c_21.java
+++ classes/c_21.java
@@ -6,31 +6,46 @@ class c_21
 {
     public static void main(String ar[])
     {
-        int arr[]={1,12,5,3,9};
+        int arr[]={1,12,0,3,9};
         int n=arr.length;
 
+        // // Minimum
+        // int min=arr[0];
+        // System.out.print("Min :- ");
+        // for(int i=0 ; i< n ; i++)
+        // {
+        //     if(min > arr[i]){
+        //         min=arr[i];
+        //     }
+        // }
+        // System.out.print(min);
+        //                                         System.out.println();
+        // // Maximum
+        // int mx=arr[0];
+        // System.out.print("Max :- ");
+        // for(int j=0 ; j< n ; j++)
+        // {
+        //     if(mx < arr[j]){
+        //         mx=arr[j];
+        //     }
+        // }
+        // System.out.print(mx);
 
-        // Minimum
-        int min=arr[0];
-        System.out.print("Min :- ");
-        for(int i=0 ; i< n ; i++)
+
+        // Move Zero
+        int j=0;
+        for(int i=0 ;i<n;i++)
         {
-            if(min > arr[i]){
-                min=arr[i];
+            if(arr[i]!=0)
+            {
+                int zero=arr[i];
+                arr[i]=arr[j];
+                arr[j]=zero;
+                j++;
             }
         }
-        System.out.print(min);
-                                                System.out.println();
-        // Maximum
-        int mx=arr[0];
-        System.out.print("Max :- ");
-        for(int j=0 ; j< n ; j++)
-        {
-            if(mx < arr[j]){
-                mx=arr[j];
-            }
-        }
-        System.out.print(mx);
+        for(int Zero:arr) System.out.print(Zero +" ");
+
     }
 }
 
