d5d5d63925df44ad7ad1078cb1593976cebcdaf3
AT-devloper <ayushap.poddar@gmail.com>
2025-06-12 17:21:05 +0530

    13 june

--- classes/c_24.java
+++ classes/c_24.java
@@ -34,18 +34,81 @@ class c_24
         System.out.println();
         if(pl) System.out.print("This is Palindrome");
         else  System.out.print("This is Not Palindrome");
-
-        
         return 0;
     }
 
+
+    static int friqancy(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {   
+            if(arr[i]!=-1)
+            {
+            int fr=1;
+            for(int j=i+1;j<arr.length;j++)
+            {   
+                if(arr[i]==arr[j])
+                {
+                fr++;
+                arr[j]=-1;
+                } }
+               System.out.println(arr[i] +" :- " +fr);
+            } }
+    return 0;
+    }
+
+    static int unique(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {   
+            boolean un=true;
+            for(int j=i+1;j<arr.length; j++)
+            {   
+                if(arr[i]==arr[j])
+                {
+                un=false;
+                arr[j]=-1;
+                }
+            }
+            if(arr[i]!=-1 && un==true)
+            {
+                    System.out.print("unique :- " +arr[i] +" ");
+            }
+        }
+        return 0;
+    }
+
+    static int duplicate(int arr[])
+    {
+        int dp=0;
+        System.out.print("Duplicate :- ");
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {   
+                if(arr[i]==arr[j]) System.out.print(arr[i] +" ");   
+            }}
+    
+        return 0;
+        }
+    
+
+
+    
+    
     public static void main(String ar[])
     {
-        int arr[]={1,2,3,4,5};
+        int arr[]={1,2,3,2,1};
         
-        sum(arr);
-        System.out.println();
-        rev(arr);
+        // sum(arr);
+        // System.out.println();
+        // rev(arr);
+        // System.out.println();
+        // friqancy(arr);
+        // System.out.println();
+        // unique(arr);
+        // System.out.println();
+        duplicate(arr);
         
     }
 }
--- classes/tempCodeRunnerFile.java
+++ classes/tempCodeRunnerFile.java
@@ -1,18 +1,4 @@
-int arr[]={1,1,1,3,4,3,4,9};    // Friquancy
-    int n=arr.length;
-    for(int i=0;i<n;i++)
-    {   
-        if(arr[i]!=-1)
-        {
-            int fr=1;
-            for(int j=i+1;j<n;j++)
-            {   
-                if(arr[i]==arr[j])
-                {
-                fr++;
-                arr[j]=-1;
-                }
-            }
-            System.out.println(arr[i] +" :- " +fr);
-        }
-    }
+sum(arr);
+        // System.out.println();
+        // rev(arr);
+        // System.out.println();
