39cd57de6d885f54449f30529043a8749d5b6235
AT-devloper <ayushap.poddar@gmail.com>
2025-07-09 10:06:52 +0530

    9july

--- Practice/Prc_7.java
+++ Practice/Prc_7.java
@@ -5,7 +5,7 @@ class Prc_7
 {
     public static void main(String ar[])
     {
-    // String str="0000123456";
+    // String str="0000123456";         //Remove Zero from string
 
     // int in=0;
     // while(in<str.length() -1 && str.charAt(in)=='0') 
@@ -15,7 +15,7 @@ class Prc_7
     
     // System.out.print(str.substring(in));
 
-    String str = "ABCD";
+    String str = "ABCD";            // Reverse String
 
     char arr[]=str.toCharArray();
     int le=0,re=str.length()-1;
