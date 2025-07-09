94a9d8c70968a321d5cd34859434bd029e1bd5a2
AT-devloper <ayushap.poddar@gmail.com>
2025-07-09 09:57:10 +0530

    9 july

---
+++ Practice/Prc_7.java
@@ -0,0 +1,34 @@
+//
+//
+
+class Prc_7
+{
+    public static void main(String ar[])
+    {
+    // String str="0000123456";
+
+    // int in=0;
+    // while(in<str.length() -1 && str.charAt(in)=='0') 
+    // {
+    //     in++;
+    // }
+    
+    // System.out.print(str.substring(in));
+
+    String str = "ABCD";
+
+    char arr[]=str.toCharArray();
+    int le=0,re=str.length()-1;
+
+    while(le<re)
+    {
+       char temp=arr[le];
+       arr[le]=arr[re];
+       arr[re]=temp;
+
+        le++;
+        re--;
+    }
+    System.out.print(arr);
+
+}}
