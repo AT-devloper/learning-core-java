a69e7beae19ddd642c7a4f9b55cbefafb6149150
AT-devloper <ayushap.poddar@gmail.com>
2025-07-12 18:34:57 +0530

    12 july

--- Java-DSA/dc_1.java
+++ Java-DSA/dc_1.java
@@ -16,12 +16,21 @@ class dc_1
     {
         public static void main(String ar[])
         {
-            Node obj=new Node(10);
-            Node obj1=new Node(20);
-            Node obj2=new Node(30);
+            Node obj1=new Node(10);
+            Node obj2=new Node(20);
+            Node obj3=new Node(30);
+            Node obj4=new Node(40);
 
-            ref.obj=obj1.ref;
-            ref.obj1=obj2.ref;
-            ref.obj2=null;
+            obj1.ref=obj2;
+            obj2.ref=obj3;
+            obj3.ref=obj4;
+            obj4.ref=null;
+
+            Node obj = obj1;        // Reference pointout = Hashcode
+                while(obj!=null)
+                {
+                    System.out.print(obj.value +" ");
+                    obj=obj.ref;
+                }
         }
     }
