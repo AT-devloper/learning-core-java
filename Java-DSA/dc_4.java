8834f3a4952f7eb292f2190d50ac3438388bc1fc
AT-devloper <ayushap.poddar@gmail.com>
2025-07-16 14:15:21 +0530

    16 july

---
+++ Java-DSA/dc_4.java
@@ -0,0 +1,31 @@
+//  16 july 2025
+//  QUEUE using Array
+
+class Que{
+    int arr[];
+    int front , rear;
+
+    Que(int n)
+    {
+        arr=new int[n];
+        front=0;
+        rear=-1;
+    }
+}
+
+class dc_4
+{
+    public static void main(String ar[])
+    {
+        Que obj = new Que(3);
+        obj.arr[++obj.rear]=10;
+        obj.arr[++obj.rear]=20;
+        obj.arr[++obj.rear]=30;
+
+        for(int i=obj.front ; i<=obj.rear ; i++)
+        {
+            System.out.println(obj.arr[i]);
+        }
+    }
+}
+
