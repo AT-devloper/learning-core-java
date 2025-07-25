6cd3d8cedb62629d9434feb5265d9c3a2254f718
AT-devloper <ayushap.poddar@gmail.com>
2025-07-25 10:12:54 +0530

    25 july

--- Practice/Prc_7.java
+++ Practice/Prc_7.java
@@ -15,21 +15,32 @@ class Prc_7
     
     // System.out.print(str.substring(in));
 
-    String str = "ABCD";            // Reverse String
+    // String str = "ABCD";            // Reverse String
 
-    char arr[]=str.toCharArray();
-    int le=0,re=str.length()-1;
+    // char arr[]=str.toCharArray();
+    // int le=0,re=str.length()-1;
 
-    while(le<re)
-    {
-       char temp=arr[le];
-       arr[le]=arr[re];
-       arr[re]=temp;
+    // while(le<re)
+    // {
+    //    char temp=arr[le];
+    //    arr[le]=arr[re];
+    //    arr[re]=temp;
 
-        le++;
-        re--;
-    }
-    System.out.println(str);
-    System.out.print(arr);
+    //     le++;
+    //     re--;
+    // }
+    // System.out.println(str);
+    // System.out.print(arr);
+
+
+
+
+    String str = "Ayush Poddar";
+    str=str.toUpperCase();
+    str = str.replace(" " ,"_");
+    System.out.print(str);
+
+
+
 
 }}
