7ed4936ea630777ec0a1f01236386b330f2a4839
AT-devloper <ayushap.poddar@gmail.com>
2025-08-25 18:29:03 +0530

    25 aug

--- Java-DSA/dc_10.java
+++ Java-DSA/dc_10.java
@@ -1,15 +1,18 @@
-//  23 Aug 2025
-//  Merge Sort
+//  25 Aug 2025
+//  
 
-class dc_9
-{   
+class dc_10
+{
+    
     static int[] div(int arr[])
-    {
+    {   
         if(arr.length<=1) return arr;
-            int mid=arr.length/2;
+
+         int mid=arr.length/2;
             int left[]= new int[mid];
             int right[]=new int[arr.length-mid];
-                
+
+         
                 for(int i=0;i<mid;i++)
                 {
                     left[i]=arr[i];
@@ -18,18 +21,21 @@ class dc_9
                 {
                     right[i-mid]=arr[i];
                 }
+
+       
             left=div(left);
             right=div(right);
         return merge(left,right);
     }
-    
-    static int[] merge(int left[],int right[]) 
+
+    static int[] merge(int left[],int right[])
     {
         int result[] = new int[left.length+right.length];
         int i=0;
         int j=0;
         int k=0;
-        while(i<left.length && j<right.length)
+
+       while(i<left.length && j<right.length)
         {
             if(left[i]<right[j])
             {
@@ -44,17 +50,18 @@ class dc_9
         {
             result[k++]=left[i++];
         }
-        while(j<right.length)
+
+        while(j<right.length)               // j
         {
             result[k++]=right[j++];
         }
-
-        return result; 
+        return result;   
     }
-    
+
     public static void main(String ar[])
     {
-        int arr[]={2,4,1,0,3};
+        int arr[]={0,5,3,1,4};
+        
         int ans[]=div(arr);
 
         for(int Ans : ans)
@@ -63,3 +70,5 @@ class dc_9
         }
     }
 }
+
+//
--- Java-DSA/tempCodeRunnerFile.java
+++ Java-DSA/tempCodeRunnerFile.java
@@ -1,14 +1,1 @@
-int arr[]={1,3,6,9,4};
-    for(int i=0;i<arr.length;i++)
-    {
-        for(int j=0;j<arr.length-i-1;j++)
-        {
-            if(arr[j+1]<arr[j])
-            {
-            int sh=arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]=sh;}
-        }
-         System.out.print(sh);
-       
-    }
+        int ans[]=div(arr);
