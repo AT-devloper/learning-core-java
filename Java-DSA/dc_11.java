1e04ff912aa5b7777641c2696f5a221b0f3cdfeb
AT-devloper <ayushap.poddar@gmail.com>
2025-08-29 11:35:50 +0530

    29 Aug

--- Java-DSA/dc_11.java
+++ Java-DSA/dc_11.java
@@ -3,10 +3,50 @@
 
 class dc_11
 {
+    static void Qs(int arr[],int high,int low)
+    {
+        if(low<high)
+        {
+        int pivotin = partition(arr,low,high);
+        Qs(arr,low,pivotin-1);
+        Qs(arr,high,pivotin+1);
+        }
+    }
+
+    static void partition(int arr[],int low ,int high)
+    {
+        int pivot=arr[high];
+        int i=low;
+
+        for(int j=low;j<high;j++)
+        {
+            if(arr[j]<pivot)
+            {
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+                i++;
+            }
+        }
+        int temp = arr[i];
+        arr[i]=arr[high];
+        arr[high]=temp;
+
+        return i;
+    }
 
     public static void main(String ar[])
     {
-            
+       int arr[]={4,5,6,7,1,3};
+       int high=arr.length-1;
+       int low=0;
+
+       Qs(arr,low,high);
+       for(int Q : Qs)
+       {
+        system.out.print(Q);
+       }
+
     }
 }
 
