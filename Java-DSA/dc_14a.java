fbfe3b41fc5c7e538d49a980b3b0ec3dc753ecf3
AT-devloper <ayushap.poddar@gmail.com>
2025-09-02 18:04:28 +0530

    2 sep

--- Java-DSA/dc_14a.java
+++ Java-DSA/dc_14a.java
@@ -1,27 +1,26 @@
-//  1 sep 2025
-//  febo with Dynamic programming
+//  2 sep 2025
+//  fibonacci   (recourson + memorization)
 
-class dc_14
+class dc_14a
 {
     static int fibo(int dp[],int n)
     {
         if(n<=1)    return n;
         else if(dp[n]!=-1)  return dp[n];
-
+        
         dp[n]=fibo(dp,n-1)+fibo(dp,n-2);
 
         return dp[n];
     }
     public static void main(String ar[])
-    {   
-        int n=5;
+    {
+        int n=4;
         int dp[]=new int[n+1];
-        
+
         for(int i=0;i<dp.length;i++)
         {
             dp[i]=-1;
         }
-        System.out.print(n +" is " +fibo(dp,n));
-
+        System.out.print(fibo(dp,n));
     }
 }
