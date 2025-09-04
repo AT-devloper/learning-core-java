7ed27783732b2fa27ea35955f49d6ab23aa1d26e
AT-devloper <ayushap.poddar@gmail.com>
2025-09-04 18:23:49 +0530

    4 sep

--- Java-DSA/dc_16.java
+++ Java-DSA/dc_16.java
@@ -1,10 +1,54 @@
 //  4 sep 2025
 //  
 
-class dc_16
+    interface Rider
+    {
+        void drive();
+    }
+
+    class bike implements Rider
+    {
+        public void drive()
+        {
+            System.out.println("Rider start Drive bike");
+        }
+    }
+    class car implements Rider
+    {
+        public void drive()
+        {
+            System.out.println("Rider start Drive car");
+        }
+    }
+    class truck implements Rider
+    {
+        public void drive()
+        {
+            System.out.println("Rider start Drive truck");
+        }
+    }
+
+    class All 
+    {
+        int ref;
+        Rider rider;
+
+        public All(Rider rider)
+        {
+            this.rider=rider;
+        }
+        public void start()
+        {
+            rider.drive();
+        }
+    }
+
+    class dc_16
 {
     public static void main(String ar[])
-    {
-        
+    {  
+        All all= new All();
+        all.start();
     }
 }
+//
