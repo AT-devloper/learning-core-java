d064d0e7769567e220e2f0ed9dbebc56c85b06ac
AT-devloper <ayushap.poddar@gmail.com>
2025-09-06 09:28:33 +0530

    5 sep

--- Java-DSA/dc_17.java
+++ Java-DSA/dc_17.java
@@ -1,11 +1,30 @@
 //  5 sep 2025
-//
+//  Back Tracking
+
+import java.util.*;
 
 class dc_17
 {
+    void genSub(int arr[],list li,int index)
+    {
+        if(index==arr.length)
+        {
+            System.out.print(li);
 
+            return ;
+        }
+
+        li.add(arr(index));
+        genSub(arr,li,index+1);
+
+        li.remove(arr(index));
+        genSub(arr,li,index+1);
+    }
+    
     public static void main(String ar[])
     {
-        
+      int arr[]={1,2,3};
+      genSub(arr,new arraylist(),0);
+
     }
 }
